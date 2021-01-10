63154d72842be8bc533a1917d75e6087cad2f5a0
yuesha-yc <kevinwangyichen@gmail.com>
2021-01-10 14:17:59 +0800

    a util

---
+++ src/main/java/com/teammoeg/elt/util/IArtist.java
@@ -0,0 +1,33 @@
+/*
+ *  Copyright (c) 2020. TeamMoeg
+ *
+ *  This file is part of Energy Level Transition.
+ *
+ *  Energy Level Transition is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  Energy Level Transition is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Energy Level Transition.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.teammoeg.elt.util;
+
+/**
+ * This is an entry point for custom debug drawing utilities to be passed through standard code.
+ */
+@FunctionalInterface
+public interface IArtist<T>
+{
+    static <T> IArtist<T> nope()
+    {
+        return (name, index, instance) -> {};
+    }
+
+    void draw(String name, int index, T instance);
+}
