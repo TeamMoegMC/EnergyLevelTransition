af2cbe390fc0810c2c55577d33cb0116d2d995b4
yuesha-yc <kevinwangyichen@gmail.com>
2021-01-17 15:11:33 +0800

    modular GUI step 1

---
+++ src/main/java/com/teammoeg/elt/client/gui/ResearchModularGui.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2020. TeamMoeg
+ *
+ *  This file is part of Energy Level Transition.
+ *
+ *  Energy Level Transition is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  Energy Level Transition is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Energy Level Transition.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.teammoeg.elt.client.gui;
+
+import com.github.zi_jing.cuckoolib.gui.IGuiHolderCodec;
+import com.github.zi_jing.cuckoolib.gui.IModularGuiHolder;
+import com.github.zi_jing.cuckoolib.gui.ModularGuiInfo;
+import net.minecraft.entity.player.PlayerEntity;
+
+public class ResearchModularGui implements IModularGuiHolder {
+    @Override
+    public IGuiHolderCodec getCodec() {
+        return null;
+    }
+
+    @Override
+    public ModularGuiInfo createGuiInfo(PlayerEntity player) {
+        return null;
+    }
+}
