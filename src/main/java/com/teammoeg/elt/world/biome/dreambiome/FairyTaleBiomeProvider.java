c8ff05da2882f5b5d3004fa866eaa9e5e1b0e84e
dashuaibia <936336596@qq.com>
2021-02-04 20:22:48 +0800

    2

---
+++ src/main/java/com/teammoeg/elt/world/biome/dreambiome/FairyTaleBiomeProvider.java
@@ -0,0 +1,54 @@
+package com.teammoeg.elt.world.biome.dreambiome;
+
+import com.google.common.collect.ImmutableList;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.teammoeg.elt.world.biome.ELTBiomes;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryLookupCodec;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.provider.BiomeProvider;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class FairyTaleBiomeProvider extends BiomeProvider {
+    private final long seed;
+    private final Registry<Biome> biomes;
+    private final Biome fallasleep;
+    public static final Codec<FairyTaleBiomeProvider> CODEC = RecordCodecBuilder.create((builder) -> {
+        return builder.group(
+                RegistryLookupCodec.create(Registry.BIOME_REGISTRY).forGetter((provider1) -> {
+                    return provider1.biomes;
+                }),
+                Codec.LONG.fieldOf("seed").forGetter((FairyTaleProvider) -> {
+                    return FairyTaleProvider.seed;
+                })).apply(builder, builder.stable(FairyTaleBiomeProvider::new));
+    });
+
+    public FairyTaleBiomeProvider(Registry<Biome> lookupRegistry, long seed) {
+        this(lookupRegistry, seed, lookupRegistry.getOrThrow(ELTBiomes.getKey(ELTBiomes.fallasleepbiome.get())));
+    }
+
+    protected FairyTaleBiomeProvider(Registry<Biome> lookupRegistry, long seed, Biome fallasleep) {
+        super(ImmutableList.of(fallasleep));
+        this.biomes = lookupRegistry;
+        this.seed = seed;
+        this.fallasleep = fallasleep;
+    }
+
+    @Override
+    protected Codec<? extends BiomeProvider> codec() {
+        return CODEC;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public BiomeProvider withSeed(long seed) {
+        return new FairyTaleBiomeProvider(this.biomes, seed, this.fallasleep);
+    }
+
+    @Override
+    public Biome getNoiseBiome(int x, int y, int z) {
+        return this.fallasleep;
+    }
+}
---
+++ src/main/java/com/teammoeg/elt/world/dimension/FairyTaleChunkGenerator.java
@@ -0,0 +1,12 @@
+package com.teammoeg.elt.world.dimension;
+
+public class FairyTaleChunkGenerator {
+/*    private static ChunkGenerator createDreamChunkGenerator(Registry<Biome> biomeRegistry, Registry<DimensionSettings> dimSettingsRegistry, long seed)
+    {
+        return new NoiseChunkGenerator(DreamBiomeProvider.DreamPreset.dreamPreset.func_242619_a(biomeRegistry, seed), seed, () ->
+        {
+            return dimSettingsRegistry.getOrThrow(GNSDimensions.DREAM_NOISE_SETTINGS);
+        });
+    }*/
+
+}
