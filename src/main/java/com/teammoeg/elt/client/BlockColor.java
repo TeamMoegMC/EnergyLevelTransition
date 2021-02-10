d1d061ce7d21c382d080e476f7cdbdafd891788d
dashuaibia <936336596@qq.com>
2021-02-10 22:53:13 +0800

    6

--- src/main/java/com/teammoeg/elt/client/BlockColor.java
+++ src/main/java/com/teammoeg/elt/client/BlockColor.java
@@ -1,14 +1,13 @@
 package com.teammoeg.elt.client;
 
 import com.teammoeg.elt.block.ELTBlocks;
-import net.minecraft.client.renderer.color.BlockColors;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.GrassColors;
 import net.minecraft.world.biome.BiomeColors;
 
 public class BlockColor {
     public static void blockcolor() {
-        BlockColors blockcolors = new BlockColors();
-        blockcolors.register((state, reader, pos, colorIn) -> {
+        Minecraft.getInstance().getBlockColors().register((state, reader, pos, colorIn) -> {
             return reader != null && pos != null ? BiomeColors.getAverageGrassColor(reader, pos) : GrassColors.get(0.5D, 1.0D);
         }, ELTBlocks.ELTGRASSBLOCK);
     }
