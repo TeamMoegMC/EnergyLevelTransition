86df107ffb5f625ffa660ce14028dda0777f19e0
dashuaibia <936336596@qq.com>
2021-04-02 23:05:40 +0800

    tp4

--- src/main/java/com/teammoeg/elt/world/dimension/ELTPlayerTeleporter.java
+++ src/main/java/com/teammoeg/elt/world/dimension/ELTPlayerTeleporter.java
@@ -1,6 +1,5 @@
 package com.teammoeg.elt.world.dimension;
 
-import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
@@ -10,7 +9,7 @@ import net.minecraft.world.server.ServerWorld;
 public class ELTPlayerTeleporter {
     public static void to(PlayerEntity entity, ServerWorld world) {
         ServerPlayerEntity playerEntity = (ServerPlayerEntity) entity;
-        BlockPos valid = validPlayerSpawnLocation(world, playerEntity.blockPosition(), 64);
+        BlockPos valid = validPlayerSpawnLocation(world, playerEntity.blockPosition());
         if (valid == null) {
             playerEntity.teleportTo(world, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), entity.yRot, entity.xRot);
         } else {
@@ -18,41 +17,17 @@ public class ELTPlayerTeleporter {
         }
     }
 
-    private static BlockPos validPlayerSpawnLocation(World world, BlockPos position, int maximumRange) {
-        int radius;
-        int outerRadius;
-        int distanceSq;
+    private static BlockPos validPlayerSpawnLocation(World world, BlockPos position) {
+
         BlockPos.Mutable currentPos = new BlockPos.Mutable(position.getX(), position.getY(), position.getZ());
 
-        for (int range = 0; range < maximumRange; range++) {
-            radius = range * range;
-            outerRadius = (range + 1) * (range + 1);
-
-            for (int y = 0; y <= range * 2; y++) {
-                int y2 = y > range ? -(y - range) : y;
-
-
-                for (int x = 0; x <= range * 2; x++) {
-                    int x2 = x > range ? -(x - range) : x;
-
-
-                    for (int z = 0; z <= range * 2; z++) {
-                        int z2 = z > range ? -(z - range) : z;
-
-                        distanceSq = x2 * x2 + z2 * z2 + y2 * y2;
-                        if (distanceSq >= radius && distanceSq < outerRadius) {
-                            currentPos.set(position.asLong(x2, y2, z2));
-                            if (world.getBlockState(currentPos.below()).canOcclude() &&
-                                    world.getBlockState(currentPos).getMaterial() == Material.AIR &&
-                                    world.getBlockState(currentPos.above()).getMaterial() == Material.AIR) {
-                                //valid space for player is found
-                                return currentPos;
-                            }
-                        }
-                    }
-                }
+        for (int Y = currentPos.getY(); Y < 256; Y++) {
+            currentPos.setY(Y);
+            if (world.getBlockState(currentPos).isAir()) {
+                return currentPos;
             }
         }
+
 
         return null;
     }
